03d684eb3a40a9fc76b99ba4ad51602d9b740190
ahyi94 <lingyunggie@gmail.com>
2021-11-10 15:27:54 +0800

    add data if empty

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "assessment",
+  database: "test",
 });
 
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -7,60 +7,76 @@ router
   .route("/")
   .get(function (req, res, next) {
     db.query("SELECT * FROM staffs", (err, result) => {
-      if (err) {
-        res.status(400).json({ message: "failed" });
+      if (result.length === 0) {
+        // res.status(400).json({ message: "No Data Found" });
+        var sql = "INSERT INTO staffs (name, monthly_salary) VALUES ?";
+        var values = [
+          ['John', '10000'],
+          ['Peter', '5000']
+        ];
+
+        db.query(sql,[values], (err, result) => {
+            if (err) {
+              res.status(400).json({ message: err });
+            }
+
+            res.status(200).json({ message: "No Data, Inserted fake data" });
+          }
+        );
       }
-      const array = [];
-      let totalTax = (rate = first = firstTax = 0);
-      result.map((r) => {
-        let salary = r.monthly_salary * 12;
-        salary = parseInt(salary)
-        if (salary <= 5000) {
-          totalTax = 0;
-        } else if (salary <= 20000) {
-          rate = 0.01;
-          first = 5000;
-          firstTax = 0;
-        } else if (salary <= 35000) {
-          rate = 0.03;
-          first = 20000;
-          firstTax = 150;
-        } else if (salary <= 50000) {
-          rate = 0.08;
-          first = 35000;
-          firstTax = 600;
-        } else if (salary <= 70000) {
-          rate = 0.14;
-          first = 50000;
-          firstTax = 1800;
-        } else if (salary <= 100000) {
-          rate = 0.21;
-          first = 70000;
-          firstTax = 4600;
-        } else if (salary <= 250000) {
-          rate = 0.24;
-          first = 100000;
-          firstTax = 10900;
-        } else if (salary <= 400000) {
-          rate = 0.245;
-          first = 250000;
-          firstTax = 46900;
-        } else if (salary <= 600000) {
-          rate = 0.25;
-          first = 400000;
-          firstTax = 83650;
-        }
+      else {
+        const array = [];
+        let totalTax = (rate = first = firstTax = 0);
+        result.map((r) => {
+          let salary = r.monthly_salary * 12;
+          salary = parseInt(salary)
+          if (salary <= 5000) {
+            totalTax = 0;
+          } else if (salary <= 20000) {
+            rate = 0.01;
+            first = 5000;
+            firstTax = 0;
+          } else if (salary <= 35000) {
+            rate = 0.03;
+            first = 20000;
+            firstTax = 150;
+          } else if (salary <= 50000) {
+            rate = 0.08;
+            first = 35000;
+            firstTax = 600;
+          } else if (salary <= 70000) {
+            rate = 0.14;
+            first = 50000;
+            firstTax = 1800;
+          } else if (salary <= 100000) {
+            rate = 0.21;
+            first = 70000;
+            firstTax = 4600;
+          } else if (salary <= 250000) {
+            rate = 0.24;
+            first = 100000;
+            firstTax = 10900;
+          } else if (salary <= 400000) {
+            rate = 0.245;
+            first = 250000;
+            firstTax = 46900;
+          } else if (salary <= 600000) {
+            rate = 0.25;
+            first = 400000;
+            firstTax = 83650;
+          }
 
-        nextTax = (salary - first) * rate;
-        totalTax = firstTax + nextTax;
-        tax_payable = array.push({
-          name: r.name,
-          salary: salary,
-          tax_payable: totalTax,
+          nextTax = (salary - first) * rate;
+          totalTax = firstTax + nextTax;
+          tax_payable = array.push({
+            name: r.name,
+            salary: salary,
+            tax_payable: totalTax,
+          });
         });
-      });
 
-      res.status(200).json({ array });
+        res.status(200).json({ array });
+      }
     });
   })
   .put(function (req, res, next) {
